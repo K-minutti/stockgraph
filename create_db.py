28b890b2eb3a1907d70d8f3b2627733fd6e29825
Kevin Minutti <minutti70@gmail.com>
2021-03-05 08:28:23 -0500

    adding the create db file

---
+++ create_db.py
@@ -0,0 +1,14 @@
+import sqlite3
+connection = sqlite.connect('app.db')
+cursor = connection.cursor()
+
+#create execute statements for creating stock table
+
+
+# create historical_price table
+
+# create strategy table
+
+# create stock_strategy table
+
+#insert into strategies

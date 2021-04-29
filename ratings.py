15df7a0aed023589e976da03690a4665c2febbe8
Kevin Minutti <minutti70@gmail.com>
2021-04-29 15:00:09 -0400

    ratings spyder

--- ratings.py
+++ ratings.py
@@ -13,24 +13,9 @@ class Ratings_Spider(scrapy.Spider):
         print("HTTP STATUS "+str(response.status))
         inner_tables = response.css("table.fullview-ratings-outer tr table")
         for table in inner_tables:
-            row = table.css("tr")
-            elems = row.css("td")
-            ratings.append(elems)
+            rows = table.xpath('tr[contains(@class, "body-table-rating")]//td//text()').extract()
+            ratings.append(rows)
         print("\n")
-        #pull text from the Selector objects in elems
-        # for table in inner_tables:
-        #     ratings.append({"date":, "status":, "institution":, "rating":, "target":})
-
-        # <tr class="body-table-rating-neutral">
-        #   <td width="120" align="left">Dec-09-20</td>
-        #   <td width="200" align="left"><b>Reiterated</b></td>
-        #   <td width="250" align="left">Wedbush</td>
-        #   <td width="250" align="left">Outperform</td>
-        #   <td width="150" align="left">$150 &rarr; $160</td>
-        # </tr>
-
-
-
 
 ratings = []
 
@@ -38,4 +23,10 @@ process = CrawlerProcess()
 process.crawl(Ratings_Spider)
 process.start()
 
-print(ratings)
+
+#we want a to call this fucntion with the symbol
+#and if there is not ratings table we exit and return the message not ratings available
+#[[1,2,3], [1,2,3]]
+#for ratings in ratings pull the data as rating[1], rating[2]
+#display in the front-end
+

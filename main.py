5d0fd1c69eec516af1dfa0799d10174ab21492e8
Kevin Minutti <minutti70@gmail.com>
2021-05-11 21:11:50 -0400

    updating apis

--- main.py
+++ main.py
@@ -89,9 +89,10 @@ def index(request: Request):
 
 
 
-
-
-
+@app.get("/search/{item_id}")
+async def read_user_item(item_id: str, needy: str):
+    item = {"item_id": item_id, "needy": needy}
+    return item
 
 
 @app.get("/stock/{symbol}")

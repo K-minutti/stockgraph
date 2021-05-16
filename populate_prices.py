48118b7306fe7c00ccdad6500b8611b9c3aa1f75
Kevin Minutti <minutti70@gmail.com>
2021-05-15 20:04:52 -0400

    db script update

--- populate_prices.py
+++ populate_prices.py
@@ -2,7 +2,7 @@ import sqlite3, config
 import numpy as np
 import tulipy as ti
 import alpaca_trade_api as tradeapi
-from datetime import date
+from datetime import date, timedelta
 
 
 API_KEY = config.API_KEY
@@ -19,6 +19,8 @@ cursor.execute("""
 
 rows = cursor.fetchall()
 
+#we need to be able to run this script every day of the week.
+
 symbols = []
 stock_dict = {}
 for row in rows:
@@ -29,11 +31,13 @@ for row in rows:
 api = tradeapi.REST(API_KEY, SECRET_KEY, base_url=BASE_URL)
 #limit per alpaca
 chunk_size = 200
+yesterday = date.today() - timedelta(days=1)
 for i in range(0, len(symbols), chunk_size):
     symbol_chunk = symbols[i:i+chunk_size]
     #after=date.today().isoformat()
+
     barsets = api.get_barset(symbol_chunk, 'day', limit=60)
- 
+    
     for symbol in barsets:
 
         recent_closes = [bar.c for bar in barsets[symbol]]
@@ -42,11 +46,12 @@ for i in range(0, len(symbols), chunk_size):
         print(f"processing symbol {symbol}")
         for bar in barsets[symbol]:
             stock_id = stock_dict[symbol]
-            if len(recent_closes) >= 50 and date.today().isoformat() == bar.t.date().isoformat():
+            #if len(recent_closes) >= 50 and date.today().isoformat() == bar.t.date().isoformat():
+            if len(recent_closes) >= 50 and yesterday.isoformat() == bar.t.date().isoformat():
                 sma_20 = ti.sma(np.array(recent_closes), period=20)[-1]
                 sma_50 = ti.sma(np.array(recent_closes), period=50)[-1]
                 rsi_14 = ti.rsi(np.array(recent_closes), period=14)[-1]
-                atr_14 = ti.atr(recent_highs, recent_lows, recent_closes, period=14)[-1]
+                atr_14 = ti.atr(np.array(recent_highs), np.array(recent_lows), np.array(recent_closes), period=14)[-1]
                 change = (recent_closes[-1] - recent_closes[-2]) / recent_closes[-2] * 100
             else:
                 sma_20, sma_50, rsi_14, atr_14, change = None, None, None, None, None

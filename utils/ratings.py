3f6d7cd150d4c4e7b07cacdb643a10c28a188e4e
Kevin Minutti <minutti70@gmail.com>
2021-06-25 21:43:34 -0400

    styling ratings item

--- utils/ratings.py
+++ utils/ratings.py
@@ -3,13 +3,13 @@ import yfinance as yf
 def get_ratings(symbol):
     ticker = yf.Ticker(symbol)
     data = ticker.recommendations
-    if data.empty:
-        return None
-    else:
+    try: 
         data = data.reset_index()
         data.iloc[::-1]
         ratings = data.to_dict('records')
         return ratings
-
+    except Exception as error:
+        print(error)
+        return None
 
 #OUTPUT : [{'Date': Timestamp('2021-05-12 12:31:45'), 'Firm': 'Roth Capital', 'To Grade': 'Buy', 'From Grade': '', 'Action': 'main'}...]

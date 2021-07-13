9c63d1fc5ff7706ebbec4d161c4b48eb7f654ee4
Kevin Minutti <minutti70@gmail.com>
2021-07-13 10:58:33 -0400

    Troubleshoot yfinance calls in study_data Had to upgrade yfinance package to avoid thread error

--- utils/study_data.py
+++ utils/study_data.py
@@ -1,8 +1,8 @@
-from pandas_datareader import data as pdr
 import yfinance as yf
-yf.pdr_override()
 
-data = pdr.get_data_yahoo("SPY", start="2017-01-01", end="2017-04-30")
+data = yf.download(tickers="PLTR", period="5y", interval="1wk")
+
+
 """
     performance -> {currYear: 52Weeks, 2020, 2019...} // 5years weekly data points
     seasonality -> {Jan: .80, Feb: .80, March: .50} //max Yrs or 10Years 120 data points percentage of time the month closed higher than it opened

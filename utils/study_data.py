8015f5ea80f77633fec15b8977ed00ffb91c22a7
Kevin Minutti <minutti70@gmail.com>
2021-07-13 12:13:12 -0400

    Define helper funcs for study_data

--- utils/study_data.py
+++ utils/study_data.py
@@ -1,20 +1,44 @@
 import yfinance as yf
 
-daily_data_3M = yf.download(tickers="PLTR", period="3mo", interval="1d") #60~ data points
-weekly_data_5Y = yf.download(tickers="PLTR", period="5y", interval="1wk") #250~ data points
-monthly_data_10Y = yf.download(tickers="PLTR", period="10y", interval="1mo") #120~ data points
 
-print("\n" , "DAILY")
-print(daily_data_3M.head())
-print("\n" , "WEEKLY")
-print(weekly_data_5Y.head())
-print("\n",  "MONTHLY")
-print(monthly_data_10Y.head())
-"""
-    performance -> {currYear: 52Weeks, 2020, 2019...} // 5years weekly data points 5 x 50 250 or so data points
+def compute_study_data(symbol):
+    daily_data_3M, weekly_data_5Y, monthly_data_10Y = get_time_series_data(symbol)
+    pass
 
-    seasonality -> {Jan: .80, Feb: .80, March: .50} //max Yrs or 10Years 120 data points percentage of time the month closed higher than it opened
+def get_time_series_data(symbol):
+    daily_data_3M = yf.download(symbol, period="3mo", interval="1d") #60~ data points
+    weekly_data_5Y =  yf.download(symbol, period="5y", interval="1wk") #250~ data points
+    monthly_data_10Y =  yf.download(symbol, period="10y", interval="1mo") #120~ data points
+    return daily_data_3M, weekly_data_5Y, monthly_data_10Y
+
+def get_performance(data):
+    """
+    return an object with keys for every year that hold values equal to an object of records where we have  {year: {date: 1, change: "",})
+    and the date corresponds to a week of data so 1 is the first week of january for any given year
+    """
+    pass
+
+def get_seasonality(data):
+    """
+    calc percentage of time price closed higher then it opened for every month for all years
+     seasonality -> {{Jan: .80, Feb: .80, March: .50} // return object with 12 key-value pairs where each month holds a percentage 
+       df create month col group by month then group by month then return new df where each row is a month and each month has a percentage col and calc 
+       X / total where X is total num of rows where row.open < row.close and total is the total nums of monthly data points in each group
+    """
+    pass
+
+def get_volatility(data):
+    """
     Volatility -> {} // Plot 5 period ATR Of Weekly - 5Y chart , Plot 14 period ATR Of daily - 3m chart, variance and stddev | for each chart plotted plot price line as well 
+    df create ATR5 Col fror Weekly5Y data and and ATR14 Col for Daily3M  same for variance col and stddev col
+    """
+    pass
+
+def get_volume_analysis(data):
+    """
     Volume -> {} //  take volume over last 3 months and identify any volume spikes, periods of largest volume and plot horizontal lines on tops/ bottoms of those candels
     is stock in a low volume period ? or high volume period.
-"""
+    
+    """
+    pass
+compute_study_data("AAPL")

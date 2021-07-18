923144c3ac53cd4dedb327ddfc07c7c5ab788136
Kevin Minutti <minutti70@gmail.com>
2021-07-18 15:05:10 -0400

    Refactor study_data analysis funcs

--- utils/study_data.py
+++ utils/study_data.py
@@ -7,6 +7,7 @@ import pandas as pd
 #MAIN- FUNCTION
 def compute_study_data(symbol):
     daily_3M, weekly_5Y, monthly_10Y = get_time_series_data(symbol)
+    
     performance = get_performance(weekly_5Y)
     seasonality = get_seasonality(monthly_10Y)
     volatility = get_volatility(weekly_5Y, daily_3M)
@@ -20,7 +21,7 @@ def get_time_series_data(symbol):
     monthly_data_10Y =  yf.download(symbol, period="10y", interval="1mo") #120~ data points
     return daily_data_3M, weekly_data_5Y, monthly_data_10Y
 
-
+#PERFORMANCE FUNC
 def get_performance(data):
     processed_data = process_data_for_performance(data)
     performance_by_year = get_performance_by_year(processed_data)
@@ -32,29 +33,25 @@ def process_data_for_performance(data):
     data = data.fillna(0)
     return data
 
-def ytd_precent_change(row, first_day_of_year_close):
-    difference= row['Close'] - first_day_of_year_close
-    change_precentage = (difference / first_day_of_year_close)*100
-    return change_precentage
+def get_five_years():
+    curr_year = date.today().year
+    years = [x for x in range(curr_year, curr_year-5, -1)]
+    return years
+
 #Helper  - @get_performance  
 def get_performance_by_year(data):
     performance_by_year = {}
     last_five_years = get_five_years()
     for year in last_five_years:
         year_df = data[data.index.year == year].copy()
-        first_day_close = year_df.iloc[0]['Close'] 
         year_df['time'] = np.arange(1, len(year_df)+1)
-        year_df['value'] = year_df.apply(lambda row: ytd_precent_change(row, first_day_close), axis=1)
-        year_df = year_df.drop(columns=['Open', 'High', 'Low', 'Close', 'Adj Close', 'Volume'])
+        year_df.rename(columns={'Close': 'value'}, inplace=True)
+        year_df = year_df.drop(columns=['Open', 'High', 'Low', 'Adj Close', 'Volume'])
         performance_by_year[year] = year_df.to_dict('records')
     return performance_by_year
 
-def get_five_years():
-    curr_year = date.today().year
-    years = [x for x in range(curr_year, curr_year-5, -1)]
-    return years
 
-
+#SEASONALITY FUNC
 def get_seasonality(data):
     months = [x for x in range(1,13)]
     seasonality_by_month = []
@@ -67,16 +64,12 @@ def get_seasonality(data):
         seasonality_by_month.append({'time': month, 'value': percentage_of_months_closed_higher})
     return seasonality_by_month
 
-def get_volatility(weeklyData, dailyData):
-    """>  {'time', 'value'}"""
-    hi = atr_series(dailyData, 7)
-    return hi
 
-def atr_series(data, period=14):
-    highs = data['High'].to_numpy()
-    lows = data['Low'].to_numpy()
-    closes = data['Close'].to_numpy()
-    atr_series = ti.atr(highs, lows, closes, period=period) #mask the data for the date starting from 
+#VOLATILITY ANALYSIS
+def get_volatility(data, period):
+    """ period : int used as calulcation period for atr, variance, and stddev"""
+    highs, lows, closes = hlc_series(data)
+    atr_series = ti.atr(highs, lows, closes, period=period)
     variance_series = ti.var(closes, period=period)
     stddev_series = ti.stddev(closes, period=period)
     series_start = len(data) - len(atr_series)
@@ -85,11 +78,22 @@ def atr_series(data, period=14):
     df_with_series['Variance'] = variance_series
     df_with_series['STD_Dev'] = stddev_series
     df_with_series['time'] = [f"{x.year}-{x.month}-{x.day}" for x in df_with_series.index] 
-    atr_ = df_with_series[["time", "ATR"]]
-    atr_.rename(columns={'ATR': 'value'}, inplace=True) 
-    atr_ = atr_.to_dict('records')
-    return  atr_
 
+
+def hlc_series(data):
+    highs = data['High'].to_numpy()
+    lows = data['Low'].to_numpy()
+    closes = data['Close'].to_numpy()
+    return highs, lows, closes
+
+
+def convert_series_to_dictrows(data, col):
+    subset = data[["time", f"{col}"]]
+    subset.rename(columns={f"{col}": "value"}, inplace=True)
+    return subset.to_dict('records')
+
+
+#VOLUME ANALYSIS
 #Helper for @get_volume_analysis
 def set_color(row):
     if row['Volume_Ratio'] > 2.5:
